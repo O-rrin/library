0e810af3f9268243fd6d0da9206200cfcff6005e
o-rrin <orrintravels@gmail.com>
2023-11-07 19:01:57 -0400

    Added the book constructor and the library class. Still need to test in the terminal.

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 'use strict'
 
-// Create the library
-const myLibrary = [];
+
 
 
 // Create the 'book' object constructor
-function Book() {
+class Book {
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = 0,
+        isRead = false
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
 
 }
 
 
-// Create function for adding book to library
-function addBookToLibrary(){
-    
+// Create a library class with built in function for searching and updating the library
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    // Create function for adding book to library, function must check if book is already in library
+    addBook(newBook) {
+    if (!this.isInLibrary(newBook)) {
+        this.myLibrary.push(newBook);
+        }
+    }
+
+    //Check if the book is in the library already
+    isInLibrary(newBook) {
+        return this.books.some((book) => book.title === title);
+    }
+
+    // Create function to remove a book from the library
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title);
+    }
+
+    // Locate and retrieve a book based on title.
+    findBook(title) {
+         return this.books.find((book) => book.title === title);
+    }
+
+
+
 }
+
+
+const dune = new Book('Dune', 'Lee Childs', 852, true)

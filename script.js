29cf310057cb91bcfcb83049e2a01f8b7308472f
o-rrin <orrintravels@gmail.com>
2023-11-18 21:10:59 -0400

    Book cards are being added and and working through JS. Flex container is overflowing and will not scroll.

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ const library = new Library();
 // Buttons
 const addBookBtn = document.querySelector(".add-button");
 const deleteAllBtn = document.querySelector(".delete");
+const addToLibrary = document.getElementById("addBtn");
 
 // Element by ID
 const inputForm = document.getElementById("pop-up");
@@ -65,4 +66,93 @@ const inputForm = document.getElementById("pop-up");
 const popUpForm = () => {
     inputForm.style.display = inputForm.style.display === 'none' ? '' : 'none';
 }
-addBookBtn.onclick = () => popUpForm();
+// addBookBtn.onclick = () => popUpForm();
+
+//Open the modal
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.btn-close')
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+addBookBtn.addEventListener("click", openModal);
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+closeModalBtn.addEventListener("click", closeModal);
+addToLibrary.addEventListener("click", closeModal);
+
+// Create a function that stores the textbox from title as the name of the book to run in the \
+//library.addbook function 
+
+const storeDetails = () => {
+    const formTitle = document.getElementById("book-title").value;
+    const formAuthor = document.getElementById("author-name").value;
+    const formPages = document.getElementById("num-of-pages").value;
+    const formRead = document.getElementById("checkBox").checked;
+    return new Book(formTitle, formAuthor, formPages, formRead);
+}
+
+// Function to clear the form after a book has been added
+const clearForm = () => document.getElementById("form").reset();
+
+
+
+
+
+
+// Next a function must create and populate the bookcard
+
+const carousel = document.querySelector(".carousel");
+
+const createBookCard = (book) => {
+    const bookCover = document.createElement("div");
+    const title = document.createElement("h3");
+    const author = document.createElement("h3");
+    const pages = document.createElement("h3");
+    const readBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+
+    bookCover.classList.add("book-cover");
+    readBtn.classList.add("btn");
+    removeBtn.classList.add("btn");
+    // Must first create the function that will change the isRead statement
+    // removeBtn.onclick = removeBook;
+
+
+    // Set the divs created above to the correct text content from the inputs
+    
+    title.textContent = `${book.title}`;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+    removeBtn.textContent = 'Remove';
+
+
+    // Append to the bookcover and carousel
+    bookCover.appendChild(title);
+    bookCover.appendChild(author);
+    bookCover.appendChild(pages);
+    bookCover.appendChild(readBtn);
+    bookCover.appendChild(removeBtn);
+    carousel.appendChild(bookCover);
+    
+
+}
+
+
+//Add to library and carousel
+const newToLibrary = () => {
+    const newBook = storeDetails();
+    library.addBook(newBook);
+    clearForm();
+
+    createBookCard(newBook);
+
+}
+addToLibrary.addEventListener("click", newToLibrary );
